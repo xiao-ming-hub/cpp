df4881a08938982d80ec11f7fd07f8cfc4c084ed
xiao-ming-hub <3154802372@qq.com>
2022-08-03 23:22:01 +0800

    update rand-string

--- rand-string.cpp
+++ rand-string.cpp
@@ -2,14 +2,14 @@
 #include <ctime>
 #include <cstdlib>
 int main(const int argc, const char **argv) {
-    int n = 10;
-    if (argc == 2) n = atoi(argv[1]);
-    srand(time(nullptr));
-    while (n--) {
-        char c = 0;
-        do c = rand() & 0x7f;
-        while (('a' > c || c > 'z') && ('A' > c || c > 'Z'));
-        putchar(c);
-    }
-    return puts(""), 0;
+  int n = 10;
+  if (argc == 2) n = std::atoi(argv[1]);
+  std::srand(std::time(nullptr));
+  while (n--) {
+    char c = 0;
+    do c = std::rand() & 0x7f;
+    while (('a' > c || c > 'z') && ('A' > c || c > 'Z'));
+    std::putchar(c);
+  }
+  return std::puts(""), 0;
 }
